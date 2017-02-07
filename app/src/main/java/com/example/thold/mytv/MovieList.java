48ac874e92305f6605ff97460b598761af1ca9d7
thold <Roalknight@gmail.com>
2017-02-07 17:33:14 +0700

    [ThoLD] Update link to display Background offline

--- app/src/main/java/com/example/thold/mytv/MovieList.java
+++ app/src/main/java/com/example/thold/mytv/MovieList.java
@@ -23,13 +23,6 @@ public final class MovieList {
 
         list = new ArrayList<Movie>();
 
-        String title[] = {
-                "ThoLD Review",
-                "Google Demo Slam_ ThoLD",
-                "Introducing Gmail Blue",
-                "Introducing Google Fiber to the Pole",
-                "Introducing Google Nose"
-        };
 
         String description = "Fusce id nisi turpis. Praesent viverra bibendum semper. "
                 + "Donec tristique, orci sed semper lacinia, quam erat rhoncus massa, non congue tellus est "
@@ -38,43 +31,95 @@ public final class MovieList {
                 + "facilisis mattis. Ut aliquet luctus lacus. Phasellus nec commodo erat. Praesent tempus id "
                 + "lectus ac scelerisque. Maecenas pretium cursus lectus id volutpat.";
 
-        String videoUrl[] = {
+        // Add movie list (category, title, description, author, videoURL, Card Image, Background Image)
+        list.add(buildMovieInfo(
+                // category
+                "category",
+                // Title
+                "ThoLD Review",
+                // Description
+                description,
+                // Author (studio)
+                "Studio Zero",
+                // Video URL
                 "http://youtu.be/Qs-XcmaxaLw?list=PLzYC8Z5DctskCLLZaYfYqIfOvjQ8WMG9L",
-                "http://hplus.com.vn/themes/front/player/jwplayer.flash.swf",
-                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue.mp4",
-                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole.mp4",
-                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose.mp4"
-        };
-
-        // Background image change when select video
-        String bgImageUrl[] = {
-                urlBgImage,
-                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/Demo%20Slam/Google%20Demo%20Slam_%2020ft%20Search/bg.jpg",
-                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue/bg.jpg",
-                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole/bg.jpg",
-                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose/bg.jpg",
-        };
-
-        // Link image card
-        String cardImageUrl[] = {
+                // Card Image
                 "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202010_%20Year%20in%20Review/card.jpg",
-                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/Demo%20Slam/Google%20Demo%20Slam_%2020ft%20Search/card.jpg",
-                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue/card.jpg",
-                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole/card.jpg",
-                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose/card.jpg"
-        };
+                // Background image
+                urlBgImage
+        ));
 
         // Add movie list (category, title, description, author, videoURL, Card Image, Background Image)
-        list.add(buildMovieInfo("category", title[0],
-                description, "Studio Zero", videoUrl[0], cardImageUrl[0], bgImageUrl[0]));
-        list.add(buildMovieInfo("category", title[1],
-                description, "Studio One", videoUrl[1], cardImageUrl[1], bgImageUrl[1]));
-        list.add(buildMovieInfo("category", title[2],
-                description, "Studio Two", videoUrl[2], cardImageUrl[2], bgImageUrl[2]));
-        list.add(buildMovieInfo("category", title[3],
-                description, "Studio Three", videoUrl[3], cardImageUrl[3], bgImageUrl[3]));
-        list.add(buildMovieInfo("category", title[4],
-                description, "Studio Four", videoUrl[4], cardImageUrl[4], bgImageUrl[4]));
+        list.add(buildMovieInfo(
+                // category
+                "category",
+                // Title
+                "Google Demo Slam_ ThoLD",
+                // Description
+                description,
+                // Author (studio)
+                "Studio Zero",
+                // Video URL
+                "http://hplus.com.vn/themes/front/player/jwplayer.flash.swf",
+                // Card Image
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/Demo%20Slam/Google%20Demo%20Slam_%2020ft%20Search/card.jpg",
+                // Background image
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/Demo%20Slam/Google%20Demo%20Slam_%2020ft%20Search/bg.jpg"
+        ));
+
+        // Add movie list (category, title, description, author, videoURL, Card Image, Background Image)
+        list.add(buildMovieInfo(
+                // category
+                "category",
+                // Title
+                "Introducing Gmail Blue",
+                // Description
+                description,
+                // Author (studio)
+                "Studio Zero",
+                // Video URL
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue.mp4",
+                // Card Image
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue/card.jpg",
+                // Background image
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue/bg.jpg"
+        ));
+
+        // Add movie list (category, title, description, author, videoURL, Card Image, Background Image)
+        list.add(buildMovieInfo(
+                // category
+                "category",
+                // Title
+                "Introducing Google Fiber to the Pole",
+                // Description
+                description,
+                // Author (studio)
+                "Studio Zero",
+                // Video URL
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole.mp4",
+                // Card Image
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole/card.jpg",
+                // Background image
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole/bg.jpg"
+        ));
+
+        // Add movie list (category, title, description, author, videoURL, Card Image, Background Image)
+        list.add(buildMovieInfo(
+                // category
+                "category",
+                // Title
+                "Introducing Google Nose",
+                // Description
+                description,
+                // Author (studio)
+                "Studio Zero",
+                // Video URL
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose.mp4",
+                // Card Image
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose/card.jpg",
+                // Background image
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose/bg.jpg"
+        ));
 
         return list;
     }
